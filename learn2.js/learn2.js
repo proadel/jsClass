8fe2e8d53d7721492e91afa62b43a5500499d529
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-11 00:18:34 +0300

    variables.js
    variable = value;

---
+++ learn2.js/learn2.js
@@ -0,0 +1,28 @@
+        /* learn2 | VARIABLES TYPES in JS:
+        ---------------------------------------------------
+        1. string = text = ('hello') or ("hello").
+        example : basic of variables
+        -----------------
+        var name = value;
+        name = value;
+        -----------------
+         var name = ('javascript class'); or without var
+         name = ('javascript class');
+        ===================================================
+        2. integer = numbers = 1 2 3 ... (2344)
+        3. float = decimal numbers = 1.3 3.76 0.22.. (3.14)
+        4. boolean = true | false .
+        5. undefined = The undefined property indicates that 
+           a variable has not been assigned a value, or not 
+           declared at all.
+
+        */
+        //print example from var -->
+        var name = ('js Class'); //string
+        var age = 43; //integer = int
+        var weight = 66.5; // float
+        var students = true; // boolean
+        document.write(name); // print command in js or:
+         document.write(name, age);
+          document.write(name, age, weight);
+            document.write(name, age, weight, students);
