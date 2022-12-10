1250b2fb600f296dd0b5ea854004e3a0903bd036
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-11 01:13:14 +0300

    Var-iable_rules.js

---
+++ learn3.js/learn3.js
@@ -0,0 +1,22 @@
+ /* learn3 | Clean VARIABLES in JS
+        basic:
+        variable = value;
+        ----------------------------------
+        1. start with upper letters is accepted in js variables.
+        2. start with lower letters is accepted in js variables.
+        3. start with $ sign is accepted in js variables.
+        4. start with _ sign is accepted in js variables.
+        5. reserved names cannot be used in js variables.
+           like : var | function | let | ...., etc.
+        6. start with numbers is not accepted in js variables, But
+           it can end with numbers or/ and use numbers any-position 
+           in var but not begins with Numbers. 
+
+        */
+         //var Name = ('1- start with upper letters is accepted in js variables.');
+         //var cname = ('2- start with lower letters is accepted in js variables.');
+         //document.write(cname);
+         //var $phone = 00967733477848;
+         //document.write($phone);
+         var _class = ('4. start with _ sign is accepted in js variables.');
+         document.write(_class);
