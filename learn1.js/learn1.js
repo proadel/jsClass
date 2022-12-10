fd5eeb9ceb1557b5cfb2e29d3d08794794b1f70f
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-11 00:04:19 +0300

    PRINT in JAVASCRIPT

---
+++ learn1.js/learn1.js
@@ -0,0 +1,20 @@
+// learn1.js
+// print
+//=====================================
+
+/* learn1.js --------------------------
+ here you can put javascript codes 
+ without need to <script></script>
+ unlike the HTML pages.
+*/
+
+//-------------------------------------
+// print with js ----------------------
+//the main code for print as :
+//document.write();
+//document.write('');
+//document.write("");
+//-------------------
+document.write('EXTERNAL: Welcome To Javascript Class 2022');
+
+
