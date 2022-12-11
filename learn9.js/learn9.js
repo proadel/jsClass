848cdf50200a65877706a0593ad13fa8313334cc
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-11 23:56:46 +0300

    browsing_methods (js)

---
+++ learn9.js/learn9.js
@@ -0,0 +1,25 @@
+// learn9.js | browsing_methods(js)
+        //------------------------------------------
+        //  All data browsing methods in javascript
+        //------------------------------------------
+        // 1. alert web-browsing box.js
+        //alert("lets learn javascript 2022");
+        //+++++++++++++++++++++++++++++++++++++++++++
+        // 2. document.write() = print cmd.js
+        //document.write('lets learn javascript 2022');
+        //+++++++++++++++++++++++++++++++++++++++++++
+        // 3. console | developers data-display mode
+        // to see it --> rightClick_learn9.html
+        // in your web-browser-->inspect--> console 
+        //console.log('lets learn javascript 2022');
+        //+++++++++++++++++++++++++++++++++++++++++++
+        // now! can you imagine the amount of FANTAZIA 
+        // of This .Example. write to me if you want.
+        var name = 'EVENTS:12DEC-DevFest Sana`a 2022 | Flutter DevPro : by Google Developer Groups meetup GDG Global, UST University-60TH STREET, Sana`a - Yemen, follow-us';
+        // - First for developers
+        //console.log(name);
+        // -- Second for web-page-browsing
+        //document.write(name);
+        // --- The last one For aler box web-page-browsing 
+        alert(name);
+        //--------------------------------------------------------------------
