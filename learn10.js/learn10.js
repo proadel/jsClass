434f892ffbe088e6a216e7a2ba4607a534806919
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-12 01:01:57 +0300

    HTML.(js) coding

---
+++ learn10.js/learn10.js
@@ -0,0 +1,5 @@
+// learn10.js | HTML.(js)coding
+      //---------------------------------------------
+      // lets learn codind inside j.script>
+      document.write('<center> Write Heading tags in javascript : <hr> <h1>lets learn coding HTML.js 2022</h1><h2>HTML.js 2022</h2><h3>HTML.js 2022</h3><h4>HTML.js 2022</h4><h5>HTML.js 2022</h5><h6>HTML.js 2022</h6s> <hr> learn and dev. javascript with Adel MD. | adeldawood@live.com (+967) 733 477-848 </center>');
+      //---------------------------------------------
