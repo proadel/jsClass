d7c6ccef6449173d5d9188052a0cd04e7c897415
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-11 20:32:19 +0300

    Text +replace.js

---
+++ learn8.js/learn8.js
@@ -0,0 +1,15 @@
+      // learn8.js | Text +replace.js
+      //---------------------------------------------
+      // -phase1
+      // We assume that incorrect information 
+      // has been entered '$', you should replaced to '-' .
+      // main code to replace strings :
+      // document.write(var_name.replace(/\/g, ''));
+      // you can use space too, to devide the texts.. whatever.
+      //---------------------------------------------
+      ///*
+      fullName = 'adel$mohammed$dawood$mohammed'
+      document.write(fullName.replace(/\$/g, '-')); 
+      //*/
+      //---------------------------------------------
+      //=============================================
