f9307fa6077914dd3933928395467a5d026b3d7c
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-11 15:54:29 +0300

    print | document.write(typeof var)

---
+++ learn4.js/learn4.js
@@ -0,0 +1,33 @@
+// learn4.js | print | document.write(typeof var)
+     // to print the type of variables
+     //--------------------------------------------
+     var name = ('Learn JS4');
+     document.write(typeof name); //print= string
+     //--------------------------------------------
+     var name2 = ("Learn JS4");
+     document.write(typeof name2); //print= string
+     //--------------------------------------------
+     var age = 43;
+     document.write(typeof age); //print= number"integer"
+     //----------------------------------------------------
+     var age2 = '43';
+     document.write(typeof age2); //print= number"integer"
+     //----------------------------------------------------
+     var weight = 66.5;
+     document.write(typeof weight); //print= number"float"
+     //----------------------------------------------------
+     var male = true;
+     document.write(typeof male); //print= boolean
+    //----------------------------------------------------
+     document.write(typeof []); //print= object
+    //----------------------------------------------------
+     document.write(typeof {}); //print= boolean
+     //---------------------------------------------------
+     document.write(typeof 44); //print= number
+     //----------------------------------------------
+     document.write(typeof '44'); //print= string
+     //----------------------------------------------
+     document.write(typeof java); //print= undefined
+     //----------------------------------------------
+     //document.write(typeof ()); //print= error
+     //----------------------------------------------
