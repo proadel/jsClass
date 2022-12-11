8ae8ac71ec70f7e45e59bf6ad48e68b8234eebd2
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-11 19:46:49 +0300

    Text +collection.js

---
+++ learn7.js/learn7.js
@@ -0,0 +1,28 @@
+// learn7.js | Text +collection.js
+      //------------------------------------
+      // -phase1
+      //------------------------------------
+      /*
+      var fname = 'adel'; // firat name
+      var lname = '   mohammed'; // last name
+      document.write(fname + lname); 
+      */
+      //------------------------------------
+      // -phase2
+      ///*
+      fname = 'adel'; // first name
+      lname = 'mohammed'; // last name
+      fullName = fname + lname;
+      document.write(fullName);
+      //*/
+      //------------------------------------
+      // -phase3
+      /*
+      fname = ' adel'; // firat name
+      lname = '  mohammed'; // last name
+      fullName = fname + lname;
+      fullName2 = lname + fname; 
+      document.write(fullName);
+      document.write(fullName2);
+      */
+      //====================================
