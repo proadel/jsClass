2f4aa480ec85cb62a10e2918abb2c656a3477a9a
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-12 02:09:15 +0300

    CSS.(js) coding

---
+++ learn11.js/learn11.js
@@ -0,0 +1,7 @@
+// learn11.js | CSS.(js) coding
+//---------------------------------------------------
+
+//document.write('lets learn CSS.js 2022');
+document.getElementById('hhh').style.color='orange';
+
+//===================================================
