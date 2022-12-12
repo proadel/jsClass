37af0ffba63d18668d73d04b0e23d890b0bbddc6
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-12 11:34:51 +0300

    callingElements.js ('CSS')

---
+++ learn12.js/learn12.js
@@ -0,0 +1,21 @@
+//learn12.js | callingElements.js('CSS')  -->
+     //reading reading as you can -->
+     //Cascading Style Sheets = CSS -->
+     //CSS is the language we use to style an HTML document.-->
+     //--------------------------------------------------------
+     // CSS- calling ==C=O=D=I=N=G============================= 
+     //1. document.getElementById('');
+        document.getElementById('h1'); // by Id
+     //========================================
+     //2. document.getElementsByClassName('');
+        document.getElementsByClassName('h2'); // by class.
+     //====================================================
+     //3. document document.getElementsByName('');
+        document.getElementsByName('p'); // by element name.
+     //-----------------------------------------------------
+     //4. document.querySelector('.div #id of h3');
+        document.querySelector('.D1V #thh');
+          // by query selector name for id of h4 
+          // inside div class name  ('class - id').
+        document.querySelector('#h4'); // by id of h4.
+     //----------------------------------------------------- 
