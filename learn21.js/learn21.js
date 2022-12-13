b2b8642e2649f8e16cd941a2fd6d40ebc37a4ddc
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-13 20:57:22 +0300

    For while_loops ( JS )

---
+++ learn21.js/learn21.js
@@ -0,0 +1,47 @@
+// learn21.js | For(JS).while_loops
+        //------------------------------------------------------------------------------------
+        //
+        /* Loops are handy, if you want to run the same code
+         over and over again, each time with a different value.
+         - Often this is the case when working with arrays.
+           # Different Kinds of Loops; JavaScript supports 
+           different kinds of loops:
+            - for - loops through a block of code a number of times
+            - for/in - loops through the properties of an object
+            - for/of - loops through the values of an iterable object
+            - while - loops through a block of code while a specified condition is true
+            - do/while - also loops through a block of code while a specified condition is true
+                   for (expression 1; expression 2; expression 3) {coding}
+               The for statement creates a loop with 3 optional expressions:
+               - Expression 1 is executed (one time) before the execution of the code block.
+               - Expression 2 defines the condition for executing the code block.
+               - Expression 3 is executed (every time) after the code block has been executed.
+        */
+        //---------------------------------------------------
+        // Example1
+        // For loop 
+        /*
+         for (i = 0 ; i < 11 ; ++i){
+            //document.write(i);
+            document.write('<li>',+ i + '</li>')
+        } 
+         */
+        //---------------------------------------------------
+          // example 2
+          //
+          /*   The While Loop
+             The while loop loops through a block of code as 
+             long as a specified condition is true.
+               while (condition) {// code}
+          */
+          // while loo
+          //-------------------------------------------------
+          /*
+          var i = 0 ;
+          while (i < 16){
+            document.write('<li>'+i+'</li>');
+            i += 3 ;
+          }
+          */
+         //----------------------------------------------------------------------------
+        //=============================================================================
