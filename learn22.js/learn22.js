7cdde9016f4e5009a9d4e36448a042cf664b15a1
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-13 23:23:49 +0300

    switchCaseBreak(js)

---
+++ learn22.js/learn22.js
@@ -0,0 +1,16 @@
+// learn22.js | switchCaseBreak(js)
+       //-----------------------------------------
+       //
+       var name = 'Medo';
+       switch (name){
+         case 'Adel':
+            alert('its Adel');
+            break;
+         case 'student1':
+            alert('yes its student1');
+            break;
+         case 'Medo':
+            document.write('Yes its Medo!');
+            break;
+         }
+         //========================================
