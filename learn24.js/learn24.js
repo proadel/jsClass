e0b985775afbd5062db4097fba7ed38e20e6d2ef
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-14 01:19:38 +0300

    alertPromptConfirm.wind(js)

---
+++ learn24.js/learn24.js
@@ -0,0 +1,17 @@
+// learn24.js | alertPromptConfirm.wind(js)
+//-------------------------------------------
+// alert - prompt - confirm
+// alert('Lets learn js alertWindows');
+//-------------------------------------
+// prompt
+//var name = prompt('Enter Your Name:');
+//document.write(`Welcome :${name}`);
+//-------------------------------------
+// confirm
+var Q = confirm('is it my learns files  of JS?');
+if (Q == true){
+    document.write('Thanks for replay');
+}
+if (Q == false){
+    document.write('Please re-check our Services!!');
+}
