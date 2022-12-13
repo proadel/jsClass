7deb41d8bfdc1bed0877385854520b4e75bf4ed7
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-14 02:59:46 +0300

    JS (1) HTML events

---
+++ learn25.js/learn25.js
@@ -0,0 +1,19 @@
+// learn25.js | JS(1)HTML.events
+        // onclick | ondblclick | onmouseover | onmouseout
+        //-------------------------------------------------
+        // onclick:
+        function onclk(){
+            alert('Lets Learn JS 2022');
+        }
+        function ondclk(){
+            alert('lets learn js on double click');
+        }
+        function onmosovr(){
+            document.getElementById('h2').style.color='red';
+        }
+        function onmoseout(){
+            document.getElementById('h3').style.color='green';
+        }
+        function onmoseout2(){
+            document.getElementById('h2').style.color='black';
+        }
