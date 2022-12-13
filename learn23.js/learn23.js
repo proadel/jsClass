27fbea3b92fe3110f8b154ad6fa6ff0853c86c6a
Adel M. Dawood <111702322+proadel@users.noreply.github.com>
2022-12-14 00:38:54 +0300

    GET-INPUT.HTML( js )

---
+++ learn23.js/learn23.js
@@ -0,0 +1,7 @@
+// learn23.js | GET-INPUT.HTML(js)
+// to save values from inputs .........
+// window vlaue GET input 
+function gt(){
+    var inp = document.getElementById('inp').value;
+    window.h4.innerHTML= '<span style="color:red">'+inp+'</span>';
+   }
